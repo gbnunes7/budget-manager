1f4bdaebe61553e56a1cff46213fff4d13e02e2d
Gabriel Melo <bielrj170@gmail.com>
2024-09-01 18:26:04 -0300

    style: finish style to operation cell

--- src/components/budgetOperations/index.tsx
+++ src/components/budgetOperations/index.tsx
@@ -10,6 +10,18 @@ interface budgetOperationProps {
 
 
 const BudgetOperations:React.FC<budgetOperationProps> = ({operation, description, price}) => {
+    
+    const getBgColor = (operation: Operation) => {
+        switch (operation) {
+            case 'Income':
+                return 'bg-green-500';
+            case 'Expense':
+                return 'bg-red-500';
+            case 'Saving':
+                return 'bg-yellow-500'
+        }
+    }
+
 	return (
 		<table className="min-w-full rounded-lg overflow-hidden">
 			<thead>
@@ -28,14 +40,13 @@ const BudgetOperations:React.FC<budgetOperationProps> = ({operation, description
 			</thead>
 			<tbody>
 				<tr className="bg-black text-left text-white text-sm font-semibold">
-					<td className="py-2 px-4">{operation}</td>
+					<td className="py-2 px-4"><span className={`px-2 py-1 rounded-sm ${getBgColor(operation)}`}>{operation}</span></td>
 					<td className="py-2 px-4">{description}</td>
 					<td className="py-2 px-4">{price}</td>
 					<td className="py-2">
 						<button><MdDelete className="text-red-700 text-2xl" /></button>
 					</td>
-				</tr>
-                
+				</tr>                
 			</tbody>
 		</table>
 	);
