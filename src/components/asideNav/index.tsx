e6d88d2e7216ca8acd2d4b79176a92cf5a88461c
Gabriel Melo <bielrj170@gmail.com>
2024-09-01 06:54:43 -0300

    feat: add a aside component with menu nav

--- src/components/asideNav/index.tsx
+++ src/components/asideNav/index.tsx
@@ -1,11 +1,17 @@
 import ListItem from "../listItem";
-import { MdAnalytics, MdDashboard } from "react-icons/md";
-import { MdOutlineCurrencyExchange } from "react-icons/md";
+import { MdAnalytics, MdDashboard, MdOutlineCurrencyExchange } from "react-icons/md";
+import Message from "../message";
+import { FaMessage } from "react-icons/fa6";
 
-const AsideNav = () => {
+
+const AsideNav:React.FC = () => {
 	return (
-		<aside className="w-[288px] flex flex-grow bg-black">
-			<nav>
+		<aside className="w-[288px] flex flex-col flex-grow bg-black gap-12 py-6">
+				<Message>
+					<FaMessage/>
+					<p>Não gaste seu dinheiro <br></br>que nem um idiota</p>
+				</Message>
+			<nav className="w-full ">
 				<ul className="flex flex-col gap-4">
 					<ListItem>
 						<MdDashboard />
