7a8d0d6bfbc69d30007501283e32c7c772b9937e
Gabriel Melo <bielrj170@gmail.com>
2024-09-01 08:29:55 -0300

    feat: add a walletCard component

---
+++ src/components/walletCard/index.tsx
@@ -0,0 +1,30 @@
+import { IconType } from "react-icons";
+
+interface WalletCardProps {
+    iconImport: IconType
+    value: number
+    dataTransfer: string
+    src: string
+    alt: string
+}
+
+const WalletCard:React.FC<WalletCardProps> = ({iconImport: Icon,value,dataTransfer,src,alt}) => {
+	return (
+		<div className="max-w-[188px] min-h-[254px] rounded-xl bg-black flex flex-col px-3.5 pt-6 gap-4">
+			<span className="text-white text-4xl">
+				<Icon/>
+			</span>
+			<div>
+				<p className="text-white text-lg font-bold">R$ {value}</p>
+				<p className="text-[#D0D0DA] ">{dataTransfer}</p>
+			</div>
+			<img
+				src={src}
+				alt={alt}
+				className="w-[160px] h-[100px]"
+			/>
+		</div>
+	);
+};
+
+export default WalletCard;
