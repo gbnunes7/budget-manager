1e444a08f12dafe72f41bcf035b5bc339d19d2fc
Gabriel Melo <bielrj170@gmail.com>
2024-09-01 05:49:31 -0300

    feat: add a userLogin component

---
+++ src/components/userLogin/index.tsx
@@ -0,0 +1,16 @@
+interface UserLoginProps {
+	nome: string;
+	src: string;
+	alt: string;
+}
+
+const UserLogin: React.FC<UserLoginProps> = ({ nome, src, alt }) => {
+	return (
+		<figure className="flex items-center gap-4">
+			<img className="w-[32px] h-[32px] rounded-full" src={src} alt={alt} />
+			<figcaption className="text-sm text-white">{nome}</figcaption>
+		</figure>
+	);
+};
+
+export default UserLogin;
