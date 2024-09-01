cad4a16ff031848ac6813cd398e4be4fb9fdac85
Gabriel Melo <bielrj170@gmail.com>
2024-09-01 05:49:16 -0300

    feat: add a header component

---
+++ src/components/header/index.tsx
@@ -0,0 +1,21 @@
+import UserLogin from "../userLogin";
+
+interface HeaderProps {
+	src: string;
+	alt: string;
+}
+
+const Header: React.FC<HeaderProps> = ({ src, alt }) => {
+	return (
+		<header className="bg-black max-h-[68px] flex justify-between items-center">
+			<img src={src} alt={alt} className="w-[100px] h-[100px]" />
+			<UserLogin
+				src="https://github.com/gbnunes7.png"
+				alt="logo"
+				nome="Gabriel"
+			/>
+		</header>
+	);
+};
+
+export default Header;
