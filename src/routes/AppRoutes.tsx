9ae3b24ac9e67c99c833f70273f4e0e3abf62429
Gabriel Melo <bielrj170@gmail.com>
2024-09-02 13:30:11 -0300

    feat: implement provider for state access across all routes

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -2,18 +2,24 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import FixedLayout from "../components/fixedLayout";
 import DashboardPage from "../pages/dashboard";
 import OperationsPage from "../pages/operations";
+import BudgetProvider from "../context";
+
 
 function AppRoutes() {
 	return (
 		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<FixedLayout />}>
-					<Route path="/" element={<DashboardPage />} />
-					<Route path="/operations" element={<OperationsPage />} />
-				</Route>
-			</Routes>
+			<BudgetProvider>
+				<Routes>
+					<Route path="/" element={<FixedLayout />}>
+						<Route path="/" element={<DashboardPage />} />
+						<Route path="/operations" element={<OperationsPage />} />
+					</Route>
+				</Routes>
+			</BudgetProvider>
 		</BrowserRouter>
 	);
 }
 
 export default AppRoutes;
+
+
