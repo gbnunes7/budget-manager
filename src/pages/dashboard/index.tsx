b0690b9046522760568396ed403e0bd9c67b55fc
Gabriel Melo <bielrj170@gmail.com>
2024-09-02 17:59:46 -0300

    refactor: replace fixed value with corresponding state

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -7,41 +7,44 @@ import { FaVault } from "react-icons/fa6";
 import MainContainer from "../../components/mainContainer";
 import AnimatedDiv from "../../components/animatedDiv";
 import BudgetOperations from "../../components/budgetOperations";
+import useBudgetContext from "../../hooks";
 
 const DashboardPage = () => {
+	const { balance, income, expense, saving } = useBudgetContext();
+
 	return (
 		<MainContainer>
 			<AnimatedDiv>
 				<div>
 					<Title>My Balance</Title>
-					<p className="text-[56px] font-bold text-white"> R$ 1.150,00</p>
+					<p className="text-[56px] font-bold text-white"> R$ {balance}</p>
 				</div>
-				<BudgetOperations/>
+				<BudgetOperations />
 				<div className="flex gap-6">
 					<WalletCard
 						iconImport={BsFillArrowDownSquareFill}
-						value={459.0}
+						value={income}
 						dataTransfer="Income"
 						src="/flowAmarelo.png"
 						alt="Image"
 					/>
 					<WalletCard
 						iconImport={GiReceiveMoney}
-						value={459.0}
+						value={expense}
 						dataTransfer="Expenses"
 						src="/flowVerde.png"
 						alt="Image"
 					/>
 					<WalletCard
 						iconImport={RiMoneyDollarBoxFill}
-						value={459.0}
+						value={balance}
 						dataTransfer="Balance"
 						src="/flowAmarelo.png"
 						alt="Image"
 					/>
 					<WalletCard
 						iconImport={FaVault}
-						value={459.0}
+						value={saving}
 						dataTransfer="Saving"
 						src="/flowVerde.png"
 						alt="Image"
