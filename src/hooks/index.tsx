b93813ec0f322a3a250307b38a7cbcca09cab202
Gabriel Melo <bielrj170@gmail.com>
2024-09-02 14:26:37 -0300

    refactor: change get fetch method to axios get

--- src/hooks/index.tsx
+++ src/hooks/index.tsx
@@ -1,16 +1,20 @@
 import { useContext, useEffect } from "react"
 import { BudgetContext } from "../context"
+import axios from "axios"
+interface IArray {
+	OperationType: string;
+	Description: string;
+	Value: number;
+}
 
 const useBudgetContext = () => {
     const {budget,setBudget} = useContext(BudgetContext)! 
     
+    const API_URL: string = 'https://sheet2api.com/v1/ChIyuf9XQFCK/budget-manager/P%C3%A1gina1?'
     // Consuming API sheet2API
     useEffect(() => {
-        fetch('https://sheet2api.com/v1/ChIyuf9XQFCK/budget-manager/P%C3%A1gina1?')
-        .then(res => res.json())
-        .then (data => {
-            setBudget(data)
-        })
+        axios.get<IArray[]>(API_URL)
+        .then(res => setBudget(res.data))
     },[setBudget])
 
     return {
