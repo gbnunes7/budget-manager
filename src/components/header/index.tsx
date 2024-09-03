3aae93bdd5c7c5b43185668c97c91e1996c29b6b
Gabriel Melo <bielrj170@gmail.com>
2024-09-03 15:22:04 -0300

    style: add a padding x to header and asidenav

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -7,7 +7,7 @@ interface HeaderProps {
 
 const Header: React.FC<HeaderProps> = ({ src, alt }) => {
 	return (
-		<header className="bg-black max-h-[68px] flex justify-between items-center">
+		<header className="bg-black max-h-[68px] flex justify-between items-center px-5">
 			<img src={src} alt={alt} className="w-[100px] h-[100px]" />
 			<UserLogin
 				src="https://github.com/gbnunes7.png"
