b8dadfaf58a1bbb5dea46b37a6ade808fddb26a1
Gabriel Melo <bielrj170@gmail.com>
2024-09-02 21:11:44 -0300

    refactor: refactor: change value input type to text for better UX and update onHandle functions, refactored the value input to type text to enhance user experience. Updated the onHandle functions stored in context to capture input values.

--- src/components/formOperation/index.tsx
+++ src/components/formOperation/index.tsx
@@ -1,66 +1,81 @@
 import { BiSolidSend } from "react-icons/bi";
 import Button from "../button";
-
-
+import useBudgetContext from "../../hooks";
 
 const FormOperation = () => {
+	const {
+		onHandleSubmit,
+		onHandleChangeOperation,
+		onHandleChangeValor,
+		onHandleChangeDescription,
+	} = useBudgetContext();
+
 	return (
-		<form
-			id="operationForm"
-			className="flex flex-col space-y-4 p-6 gap-4 bg-black text-white rounded-lg shadow-md"
-		>
-			<div className="flex flex-col">
-				<label htmlFor="operations" className="mb-2 text-lg font-semibold">
-					Operation Type
-				</label>
-				<select
-					id="operations"
-					name="Operation Type"
-					className="p-2 bg-[#0F0F13] text-[#8A8A98] rounded focus:outline-none focus:ring-2 focus:ring-[#4807EA]"
-				>
-					<option value="income">Income</option>
-					<option value="expense">Expense</option>
-					<option value="saving">Saving</option>
-				</select>
-			</div>
+		<>
+			<form
+				id="operationForm"
+				className="flex flex-col space-y-4 p-6 gap-4 bg-black text-white rounded-lg shadow-md"
+				onSubmit={onHandleSubmit}
+			>
+				<div className="flex flex-col">
+					<label htmlFor="operations" className="mb-2 text-lg font-semibold">
+						Operation Type
+					</label>
+					<select
+						id="operations"
+						name="Operation Type"
+						className="p-2 bg-[#0F0F13] text-[#8A8A98] rounded focus:outline-none focus:ring-2 focus:ring-[#4807EA]"
+						onChange={onHandleChangeOperation}
+					>
+						<option value="" disabled>
+							Select an option
+						</option>
+						<option value="Income">Income</option>
+						<option value="Expense">Expense</option>
+						<option value="Saving">Saving</option>
+					</select>
+				</div>
 
-			<div className="flex flex-col">
-				<label htmlFor="description" className="mb-2 text-lg font-semibold">
-					Description
-				</label>
-				<input
-					type="text"
-					id="description"
-					name="Description"
-					placeholder="Ex: Shopping at the mall"
-					required
-					minLength={3}
-					maxLength={120}
-					className="p-2 bg-[#0F0F13] text-[#8A8A98] rounded focus:outline-none focus:ring-2 focus:ring-[#4807EA]"
-				/>
-			</div>
+				<div className="flex flex-col">
+					<label htmlFor="description" className="mb-2 text-lg font-semibold">
+						Description
+					</label>
+					<input
+						type="text"
+						id="description"
+						name="Description"
+						placeholder="Ex: Shopping at the mall"
+						required
+						minLength={3}
+						maxLength={120}
+						className="p-2 bg-[#0F0F13] text-[#8A8A98] rounded focus:outline-none focus:ring-2 focus:ring-[#4807EA]"
+						onChange={onHandleChangeDescription}
+					/>
+				</div>
 
-			<div className="flex flex-col">
-				<label htmlFor="price" className="mb-2 text-lg font-semibold">
-					Price
-				</label>
-				<input
-					type="text"
-					id="price"
-					name="Value"
-					placeholder="Ex: 4.99"
-					required
-					minLength={3}
-					maxLength={120}
-					pattern="^\d*\.?\d+$"
-					className="p-2 bg-[#0F0F13] text-[#8A8A98] rounded focus:outline-none focus:ring-2 focus:ring-[#4807EA]"
-				/>
-			</div>
-            <Button type="submit" onClick={(event) => {
-				event.preventDefault()
-				alert('Submit')
-			}}><BiSolidSend/>Submit</Button>
-		</form>
+				<div className="flex flex-col">
+					<label htmlFor="price" className="mb-2 text-lg font-semibold">
+						Price
+					</label>
+					<input
+						type="text"
+						id="price"
+						name="Value"
+						placeholder="e.g., 4.99"
+						required
+						className="p-2 bg-[#0F0F13] text-[#8A8A98] rounded focus:outline-none focus:ring-2 focus:ring-[#4807EA]"
+						onChange={onHandleChangeValor}
+						aria-label="Price"
+						aria-required="true"
+						title="Enter the price in dollars, e.g., 4.99"
+					/>
+				</div>
+				<Button type="submit">
+					<BiSolidSend />
+					Submit
+				</Button>
+			</form>
+		</>
 	);
 };
 
