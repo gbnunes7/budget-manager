6e139b52149ab7398077b7d98661dc8cb6dfecec
Gabriel Melo <bielrj170@gmail.com>
2024-09-03 17:25:43 -0300

    component: add a analytics page

---
+++ src/pages/analytics/index.tsx
@@ -0,0 +1,15 @@
+import AnimatedDiv from "../../components/animatedDiv"
+import FinanceChart from "../../components/chart"
+import MainContainer from "../../components/mainContainer"
+
+const Analytics = () => {
+    return (
+        <MainContainer>
+            <AnimatedDiv>
+                <FinanceChart/>
+            </AnimatedDiv>
+        </MainContainer>
+    )
+}
+
+export default Analytics
