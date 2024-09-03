dda1702e227dea34c9a29f45d95c835968b3c4b7
Gabriel Melo <bielrj170@gmail.com>
2024-09-02 23:57:47 -0300

    feat: add a title

--- src/pages/operations/index.tsx
+++ src/pages/operations/index.tsx
@@ -1,11 +1,13 @@
 import AnimatedDiv from "../../components/animatedDiv";
 import FormOperation from "../../components/formOperation";
 import MainContainer from "../../components/mainContainer";
+import Title from "../../components/title";
 
 const OperationsPage = () => {
 	return (
 		<MainContainer>
 			<AnimatedDiv>
+				<Title>Add your operation here, and start managing your finances!</Title>
 				<FormOperation />
 			</AnimatedDiv>
 		</MainContainer>
