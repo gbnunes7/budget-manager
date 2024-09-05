4824b0f9d3db4521e99a24683f4783a267dd4491
Gabriel Melo <bielrj170@gmail.com>
2024-09-05 15:50:23 -0300

    feat: add a auth prvider, and a private element

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -2,32 +2,36 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import FixedLayout from "../components/fixedLayout";
 import DashboardPage from "../pages/dashboard";
 import OperationsPage from "../pages/operations";
-import BudgetProvider from "../context";
+import BudgetProvider from "../context/budgetContext";
 import Wallet from "../pages/wallet";
 import Analytics from "../pages/analytics";
-import Title from "../components/title";
+import Login from "../pages/login";
+import { ComponentType } from "react";
+import { AuthProvider } from "../context/authContext";
+import useAuth from "../hooks/useAuth/intex";
+
+const Private = ({ Element }: { Element: ComponentType }) => {
+	const { signed } = useAuth();
+
+	return signed ? <Element /> : <Login />;
+};
 
 function AppRoutes() {
 	return (
 		<BrowserRouter>
 			<BudgetProvider>
-				<Routes>
-					<Route path="/" element={<FixedLayout />}>
-						<Route path="/" element={<Wallet />} />
-						<Route path="/operations" element={<OperationsPage />} />
-						<Route path="/dashboard" element={<DashboardPage />} />
-						<Route path="/analytics" element={<Analytics />} />
-						<Route
-							path="*"
-							element={
-								<div className="bg-[#090909] h-full w-full p-14">
-									<Title>Error 404</Title>
-									<p className="text-white mt-4 text-[50px]">Página não encontrada</p>
-								</div>
-							}
-						/>
-					</Route>
-				</Routes>
+				<AuthProvider>
+					<Routes>
+						<Route path="/" element={<Login />} />
+						<Route path="/" element={<Private Element={FixedLayout} />}>
+							<Route path="wallet" element={<Wallet />} />
+							<Route path="operations" element={<OperationsPage />} />
+							<Route path="dashboard" element={<DashboardPage />} />
+							<Route path="analytics" element={<Analytics />} />
+						</Route>
+						<Route path="*" element={<Login />} />
+					</Routes>
+				</AuthProvider>
 			</BudgetProvider>
 		</BrowserRouter>
 	);
