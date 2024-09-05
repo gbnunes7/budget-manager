fd94c4a84e2a11da33abe937cc48a1c8b36a115d
Gabriel Melo <bielrj170@gmail.com>
2024-09-04 22:14:17 -0300

    feat: add a error404 page

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -5,6 +5,7 @@ import OperationsPage from "../pages/operations";
 import BudgetProvider from "../context";
 import Wallet from "../pages/wallet";
 import Analytics from "../pages/analytics";
+import Title from "../components/title";
 
 function AppRoutes() {
 	return (
@@ -16,6 +17,15 @@ function AppRoutes() {
 						<Route path="/operations" element={<OperationsPage />} />
 						<Route path="/dashboard" element={<DashboardPage />} />
 						<Route path="/analytics" element={<Analytics />} />
+						<Route
+							path="*"
+							element={
+								<div className="bg-[#090909] h-full w-full p-14">
+									<Title>Error 404</Title>
+									<p className="text-white mt-4 text-[50px]">Página não encontrada</p>
+								</div>
+							}
+						/>
 					</Route>
 				</Routes>
 			</BudgetProvider>
